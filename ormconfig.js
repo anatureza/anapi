31c410037981397806e1b1ec0faf6f92b0e84913
Gustavo Dantas Galote <gusgalote@outlook.com>
2021-10-23 16:22:41 -0300

    heroku deploy

--- ormconfig.js
+++ ormconfig.js
@@ -4,11 +4,7 @@ dotenv.config();
 
 module.exports = {
   type: "postgres",
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  url: process.env.DATABASE_URL,
   synchronize: true,
   migrations: ["src/database/migrations/*.ts"],
   entities: ["src/entities/*.ts"],
